56a6a29419193230187ccffe18a2856b437c6bc2
jppurpleheart <josepablomachucagonzalez@gmail.com>
2022-02-18 14:06:04 +0100

    Cambio mostrarPosicion

--- views/js/resultado.js
+++ views/js/resultado.js
@@ -7,10 +7,11 @@ $(document).ready(function() {
         type:  'post',
         data: {'idPartida': sessionStorage.getItem('idProvincia')},
         success: function (response) {
-            response = JSON.stringify(response);
             response = JSON.parse(response);
-            if(response.id==response.idPartida) {
-                $("#mostrarPosicion").html("Enoraguena " + response.mote + " âh ganao " + response.puntos + " puntô te âh ganao una Çigala y un hamón roça imbiçible que êttamô en criçî; la prôççima bêh puedê açêl-lo mehôh; que tû compañero êttan çacando mehorê puntuaçionê que tú. Êttâh en la poçiçión " + response.rownum + " y todabia puedê çubîh mâh çacando mehôh puntuaçión la prôççima bêh çige hugando pa podêh çubîh en el ranking.");
+            for(i = 0; i < response.length; i ++){
+                if(response[i][1]==response[0][0]) {
+                    $("#mostrarPosicion").html("Enoraguena " + response[i][2] + " âh ganao " + response[i][3] + " puntô te âh ganao una Çigala y un hamón roça imbiçible que êttamô en criçî; la prôççima bêh puedê açêl-lo mehôh; que tû compañero êttan çacando mehorê puntuaçionê que tú. Êttâh en la poçiçión " + response[i][0] + " y todabia puedê çubîh mâh çacando mehôh puntuaçión la prôççima bêh çige hugando pa podêh çubîh en el ranking.");
+                }
             }
         }
     });
